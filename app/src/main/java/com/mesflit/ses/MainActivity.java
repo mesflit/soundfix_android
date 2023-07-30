dfb43221dd630ab88d1f7e6e169595cc7751759e
mesflit <benomerfaruk11@gmail.com>
2023-07-30 14:46:01 +0300

    inititial commit

--- app/src/main/java/com/mesflit/ses/MainActivity.java
+++ app/src/main/java/com/mesflit/ses/MainActivity.java
@@ -1,14 +1,18 @@
-
 package com.mesflit.ses;
 
 import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -18,10 +22,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
     private static final int RECORD_AUDIO_PERMISSION_REQUEST_CODE = 101;
+    private static final String TAG = "MainActivity";
     private Button startButton;
     private Button stopButton;
+    private static final int BUFFER_SIZE = 2048; // Örnek bir önbellek boyutu
+    private byte[] audioBuffer = new byte[BUFFER_SIZE];
+    private int bytesRead;
+    private boolean isRecording = false;
+    private AudioRecord audioRecorder;
+    private int sampleRate = 44100;
+    private int channelConfig = AudioFormat.CHANNEL_IN_MONO;
+    private int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+    private int bufferSize;
+    private File outputFile;
+    private DataOutputStream dataOutputStream;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -107,10 +128,65 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void startBackgroundService() {
-        startService(new Intent(this, MyBackgroundService.class));
+        bufferSize = AudioRecord.getMinBufferSize(sampleRate, channelConfig, audioFormat);
+        audioRecorder = new AudioRecord(MediaRecorder.AudioSource.MIC, sampleRate, channelConfig, audioFormat, bufferSize);
+        audioRecorder.startRecording();
+        isRecording = true;
+
+        // Create the output file for saving audio data
+        String filePath = getExternalFilesDir(null).getAbsolutePath() + "/recorded_audio.pcm";
+        outputFile = new File(filePath);
+
+        try {
+            dataOutputStream = new DataOutputStream(new FileOutputStream(outputFile));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                writeAudioDataToFile();
+            }
+        }).start();
     }
 
     private void stopBackgroundService() {
-        stopService(new Intent(this, MyBackgroundService.class));
+        if (!isRecording) {
+            return;
+        }
+
+        isRecording = false;
+        audioRecorder.stop();
+        audioRecorder.release();
+        audioRecorder = null;
+
+        try {
+            dataOutputStream.close();
+            dataOutputStream = null;
+            audioBuffer = null;
+            bytesRead = 0;
+            Log.d(TAG, "Recording stopped. Audio file saved at: " + outputFile.getAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Ses çıkışını tekrar normal moda (MODE_NORMAL) geri getir
+        AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        audioManager.setMode(AudioManager.MODE_NORMAL);
+        audioManager.setSpeakerphoneOn(true); // Hoparlörü açık, normal ses çıkışı
+    }
+
+    private void writeAudioDataToFile() {
+        while (isRecording) {
+            bytesRead = audioRecorder.read(audioBuffer, 0, BUFFER_SIZE);
+            if (bytesRead != AudioRecord.ERROR_INVALID_OPERATION && bytesRead > 0) {
+                try {
+                    dataOutputStream.write(audioBuffer, 0, bytesRead);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
